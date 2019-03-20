c7a22dc9aaa3a51dece95fdbe0c7e15365abff2b
Judy Huynh <jhuynh@mossbeachhomes.com>
2019-03-20 11:40:54 -0700

    adding seeds, validation error

---
+++ src/db/seeds/20190320171308-user.js
@@ -0,0 +1,34 @@
+'use strict';
+'use strict';
+const faker = require("faker");
+
+let users = [{
+     id: 1,
+     username: "testuser1",
+     email: faker.internet.email(),
+     password: "password1",
+     createdAt: new Date(),
+     updatedAt: new Date(),
+     role: "standard"
+     },
+     {
+     id: 2,
+     username: "testuser2",
+     email: faker.internet.email(),
+     password: "password2",
+     createdAt: new Date(),
+     updatedAt: new Date(),
+     role: "standard"
+     }
+]
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+
+    return queryInterface.bulkInsert("Users", users, {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+
+    return queryInterface.bulkDelete("Users", null, {});
+  }
+};
