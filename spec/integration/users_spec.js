5f2dadd170edfecb2fe652e7baba3e5fa63b2060
Judy Huynh <jhuynh@mossbeachhomes.com>
2019-04-02 08:45:42 -0700

    added tests for private wikis

--- spec/integration/users_spec.js
+++ spec/integration/users_spec.js
@@ -33,32 +33,59 @@ describe("routes : users", () => {
 
   describe("POST /users", () => {
 
-  it("should create a new user with valid values and redirect", (done) => {
+      it("should create a new user with valid values and redirect", (done) => {
 
-    const options = {
-      url: base + "sign_up",
-      form: {
-        email: "user@example.com",
-        password: "123456789"
-      }
-    }
+        const options = {
+          url: base,
+          form: {
+            username: 'user example',
+            email: "user@example.com",
+            password: "123456789",
+            id: 1
+          }
+        }
 
-    request.post(options,
-      (err, res, body) => {
+        request.post(options,
+          (err, res, body) => {
+            User.findOne({where: {email: "user@example.com"}})
+            .then((user) => {
+              expect(user).not.toBeNull();
+              expect(user.email).toBe("user@example.com");
+              expect(user.id).toBe(1);
+              done();
+            })
+            .catch((err) => {
+              console.log(err);
+              done();
+            });
+          }
+        );
+      });
 
-        User.findOne({where: {email: "user@example.com"}})
-        .then((user) => {
-          expect(user).not.toBeNull();
-          expect(user.email).toBe("user@example.com");
-          expect(user.id).toBe(1);
-          done();
-        })
-        .catch((err) => {
-          console.log(err);
-          done();
-        });
-      }
-    );
-  });
+      it("should not create a new user with invalid attributes and redirect", (done) => {
+        request.post(
+          {
+            url: base,
+            form: {
+              email: "no",
+              password: "123456789"
+            }
+          },
+          (err, res, body) => {
+            User.findOne({where: {email: "no"}})
+            .then((user) => {
+              expect(user).toBeNull();
+              done();
+            })
+            .catch((err) => {
+              console.log(err);
+              done();
+            });
+          }
+        );
+      });
+
+    });
+
 
 });
